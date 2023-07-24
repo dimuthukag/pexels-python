ed677dba0fddf29b3f7adabd1428bc04663ac1e7
Dimuthu Gunawardhana <dimuthukag@gmail.com>
2023-07-24 09:19:50 +0530

    feat: add features to handle Photos with Pexels API
    Implement class methods in Pexels class to enable functionality for 'search photos',
    'receive curated photos', and 'get photo with a given ID'.

--- src/pexels_python/core.py
+++ src/pexels_python/core.py
@@ -1,3 +1,7 @@
+import requests
+import pexels_python._validation as _validation
+from pexels_python._resource import Photo
+
 class Pexels:
     def __init__(self, api_key:str) -> None:
         self.__api_key = api_key
@@ -5,9 +9,52 @@ class Pexels:
             'Authorization': self.__api_key
         }
     
-    # method for search photos
-    # method for recieve curated photos
-    # method for get photo with specific ID
+    def search_photos(self, query:str, orientation:str=None,size:str=None,color:str=None,locale:str=None,page:int=1,per_page:int=15)->list[Photo]:
+        """
+            Retrive a list of photos from given parameters
+        """
+        endpoint_url = f'https://api.pexels.com/v1/search'
+        if _validation.validate_page(page):
+            endpoint_url += f'?page={page}'
+        if _validation.validate_per_page(per_page):
+            endpoint_url += f'&per_page={per_page}'
+        if _validation.validate_query(query):
+            endpoint_url += f'&query={query.replace(" ","%20")}'
+        if  orientation and _validation.validate_orientation(orientation):
+            endpoint_url += f'&orientation={orientation}'
+        if size and _validation.validate_photo_size(size):
+            endpoint_url += f'$size={size}'
+        if color and _validation.validate_color(color):
+            endpoint_url += f'$color={color}'
+        if locale and _validation.validate_locale(locale):
+            endpoint_url += f'$locale={locale}'
+        
+        response = requests.get(endpoint_url,headers=self.__headers)
+        if response.status_code == 200:
+            return [Photo(resource_data) for resource_data in response.json()['photos']]
+    
+    def curated_photos(self,page:int=1,per_page:int=15)->list[Photo]:
+        """
+            Retrive a list of real time photos curated by the Pexels team
+        """
+        endpoint_url = 'https://api.pexels.com/v1/curated'
+        if _validation.validate_page(page):
+            endpoint_url += f'?page={page}'
+        if _validation.validate_per_page(page):
+            endpoint_url += f'&per_page={per_page}'
+
+        response = requests.get(endpoint_url,headers=self.__headers)
+        if response.status_code == 200:
+            return [Photo(resource_data) for resource_data in response.json()['photos']]
+
+    def get_photo(self, photo_id:int)->Photo:
+        """
+            Retrive a specific photo from its id.
+        """
+        endpoint_url = f'https://api.pexels.com/v1/photos/{photo_id}'
+        response = requests.get(endpoint_url,headers=self.__headers)
+        if response.status_code == 200:
+            return Photo(response.json())
 
     # method for search videos
     # method for recieve current popular videos
